2caae76dacfd55b0f517ed78ea4b1adeaece2817
MovingJu <baramishe@gmail.com>
2025-03-22 15:42:55 +0900

    Make commands executable

--- libs/Kbg.py
+++ libs/Kbg.py
@@ -1,20 +1,39 @@
+import pandas as pd
+
+import libs
+
 class Kbg:
     """모든 클래스에 대해 상속 받는 클래스
     모든 채팅방의 공통기능을 구현한 곳이자 카카오톡 분석기의 필수기능이 있는 곳"""
 
-    def __init__(self, path):
-        self.path = path
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
         self.commands = {
             "\\cry": self.cry,
             "\\swear": self.swear,
             "\\laugh": self.laugh
         }
+        return
+
+    def execute_command(self, user_input):
+        try: 
+            self.commands[user_input]()
+            return
+        except:
+            raise libs.Kbg_errors('그런 명령어는 없습니다.')
         
+    def execute_text(self, user_input):
+
+        return
+
     def cry(self):
         print('ㅠㅠ')
+        return
 
     def swear(self):
         print('*$&%#')
+        return
 
     def laugh(self):
         print('kiki')
+        return
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-# to do:: 시작시, 사용자가 직접 입력한 경우, preprocess하고 저장해서 실행하는 코드 만들기.
+# to do:: cry, swear등 명령어 처리 메서드 완성하기, 동일 기능 함수로 잘 빼서 만들어!!
+# reference:: https://github.com/Tanat05/korcen-ml  <- 모델 참고해서 욕설 탐지 만들기!
+## to do:: 시작시, 사용자가 직접 입력한 경우, preprocess하고 저장해서 실행하는 코드 만들기.
 
 import pandas as pd
 import glob
@@ -8,10 +10,6 @@ import libs, datas
 def main()->None:
     """전역변수의 사용을 줄이기 위해 정의한 메인 함수"""
 
-    kbg_classes = ['필수 기능만', '사무적인 채팅방 기능', '잡담하는 채팅방 기능']
-
-
-    files = glob.glob(datas.where_csv + '/*')
     files_name = files.copy()
     if (len(files) == 0):
         raise libs.Kbg_errors('파일이 없습니다!!')
@@ -31,11 +29,11 @@ def main()->None:
     
 
     print("어떤 기능들을 사용할지 알려주세요")
-    for idx, value in enumerate(kbg_classes):
+    for idx, value in enumerate(kbg_classes_keys):
         print(f"{idx} : {value}")
     
     user_input_classes = input('>>> ')
-    user_input_classes = libs.is_input_valid(user_input_classes, kbg_classes)
+    user_input_classes = libs.is_input_valid(user_input_classes, kbg_classes_keys)
     if user_input_classes == -1:
         raise libs.Kbg_errors('입력이 유효하지 않습니다!')
     
@@ -51,24 +49,35 @@ def main()->None:
 
 
     print('카카오톡 분석기 실행됨!')
-    execute_kbg(df, user_input, user_input_classes)
+    execute_kbg(df, user_input_classes)
 
     return
 
 
-def execute_kbg(df: pd.DataFrame, user_input: int, user_input_classes: int) -> None:
+def execute_kbg(df: pd.DataFrame, user_input_classes: int) -> None:
     """재귀 형태로 카카오톡 분석기를 계속 실행시킬 함수"""
 
     def inner():
-        user_input = input("$ ")
-        if user_input == '\\break':
+        user = input("$ ").strip()
+        if user == '\\stop':
             return
         
-        print('Uhaha')
+        try:
+            if user[0] == '\\':
+                malloc.execute_command(user)
+            else:
+                malloc.execute_text(user)
+
+        except libs.Kbg_errors as e:
+            print(e)
+            
 
         inner()
 
-    print('\\break 를 입력하면 종료됩니다.')
+    malloc = kbg_classes_values[user_input_classes](df)
+    del df
+
+    print('\\stop 을 입력하면 종료됩니다.')
     inner()
 
     return
@@ -77,6 +86,11 @@ def execute_kbg(df: pd.DataFrame, user_input: int, user_input_classes: int) -> N
 if __name__ == "__main__":  
 
     try: 
+
+        kbg_classes_keys = ['필수 기능만', '사무적인 채팅방 기능', '잡담하는 채팅방 기능']
+        kbg_classes_values = [libs.Kbg, libs.Formal, libs.Casual]
+
+        files = glob.glob(datas.where_csv + '/*')
         main()
         exit()
 
@@ -88,6 +102,6 @@ if __name__ == "__main__":
         print('파일이 이상합니다 다시 확인해주세요!')
         exit()
 
-    except Exception as e:
-        print('예측하지 못한 에러 발생!:', e)
-        exit()
+    # except Exception as e:
+    #     print('예측하지 못한 에러 발생!:', e)
+    #     exit()
